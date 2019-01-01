b98230d43d9bb88a0776d411da68c58c91bc0ff8
sanatnayar <snaya091@uottawa.ca>
2019-01-01 15:21:22 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
 
   render() {
     return (
+      
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
